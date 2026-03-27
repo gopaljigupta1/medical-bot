ec4ab14c6e1851ee74bdec0158070fdd94facd2e
gopaljigupta1 <gji777906@gmail.com>
2026-03-27 11:44:35 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir -p scr
+mkdir -p research
+
+touch scr/__init__.py
+touch scr/helper.py
+touch scr/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+echo = "directory or file created successfully"
